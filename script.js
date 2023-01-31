7e290af15a5d647b02eb36d595dc837538e84bef
vinaypree <122054766+vinaypree@users.noreply.github.com>
2023-01-31 16:12:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let music = new Audio("music.mp3")
+let audioTurn = new Audio("ting.mp3")
+let gameover = new Audio("gameover.mp3")    
+let turn = "X"
+let isgameover = false ;
+
+//function to chNGE turn
+const changeTurn = ()=>{
+    return turn === "X" ? "O" : "X" 
+}
+
+//function to check for a win
+const checkWin = ()=>{
+    let boxtexts = document.getElementsByClassName("boxtext");
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+     
+    wins.forEach(e =>{
+           if((boxtexts[e[0]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[2]].innerText === boxtexts[e[1]].innerText) && (boxtexts[e[0]].innerText !== ""))
+           {
+            document.querySelector('.info').innerText = boxtexts[e[0]].innerText +"  "+ "Won" 
+            isgameover = true ;
+            document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "200px"
+           } 
+    })
+
+}
+
+
+//game Logic
+let boxes = document.getElementsByClassName("box");
+// console.log(boxes);
+Array.from(boxes).forEach(element =>{
+    let boxtext = element.querySelector('.boxtext');
+    element.addEventListener('click', ()=> {
+        if(boxtext.innerText === '')
+        {
+            boxtext.innerText = turn;
+           turn = changeTurn();
+            audioTurn.play();
+            checkWin();
+            if(!isgameover)
+            {
+            document.getElementsByClassName("info")[0].innerText = "Turn for" +" "+turn ;
+            }
+        
+
+        }
+
+    })
+
+
+})
+
+//add onclick on reset button
+reset.addEventListener('click',(element)=>{
+    let boxtexts = document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element =>{
+        element.innerText = ""
+    })
+    turn = "X"
+    isgameover = false
+    document.getElementsByClassName("info")[0].innerText = "Turn for" +" "+ turn ;
+    document.querySelector('.imgBox').getElementsByTagName('img')[0].style.width = "0px"
+
+})
